7189d54bc6739bc35bbedbe86ee44c1a26a7bc8b
EmilDzh <126710982+EmilDzh@users.noreply.github.com>
2023-10-12 23:37:15 +0200

    HW 06.10.23

--- hw -29 09 23.sql
+++ hw -29 09 23.sql
@@ -239,6 +239,288 @@ insert into goods_two (title,quantity,price,in_stock)
     
     SELECT * FROM students;
     
+    -- INNER JOIN
+    
+    use learn;
+    
+    CREATE TABLE stud (
+    id integer primary key auto_increment,
+    name varchar(128) not null,
+    surname varchar(128) not null
+    );
+    
+    CREATE TABLE ages (
+    student_id integer unique,
+    age integer
+    );
+    
+   INSERT INTO stud (name , surname) VALUES ('ALEX', 'ALEXEEV'),
+											('Oleg', 'Olegov'),
+											('Maxim', 'Maximov');
+                                            
+   SELECT * FROM stud;    
+   
+   /*
+   Alex Alexeev 20 id 1 Algo
+   Oleg Olegov 34  id 2 Java
+   Maxim Maximov 25 id 3 Sql
+   */
+   
+   INSERT INTO ages (student_id, age) VALUES (1,20),(2,34),(3,25);
+   
+     INSERT INTO ages (student_id, age) VALUES (5,54);
+   
+   SELECT * FROM ages;
+   
+   INSERT INTO stud (name , surname) VALUES ('Petr', 'Petrov');
+   
+   SELECT 
+   t1.id,
+   t1.name,
+   t1.surname,
+   t2.age
+   FROM stud t1 
+   INNER JOIN ages t2
+   ON t1.id = t2.student_id;
+   
+     /*
+   Alex Alexeev 20 id 1 Algo
+   Oleg Olegov 34  id 2 Java
+   Maxim Maximov 25 id 3 Sql
+   */
+   
+   CREATE TABLE curses (
+   id integer primary key auto_increment,
+   name varchar(128)
+   );
+   
+   INSERT INTO curses (name) VALUES ('JAVA'), ('ALGO'), ('SQL');
+   
+    INSERT INTO curses (name) VALUES ('JS');
+   
+   SELECT * FROM curses;
+   
+   CREATE TABLE course2student (
+   student_id integer,
+   course_id integer
+   );
+    /*
+    id 1 - JAVA, ALGO
+    id 2 - ALGO
+    id 3 - SQL, ALGO
+    
+    */
+    
+    INSERT INTO course2student (student_id, course_id) VALUES (1,1),(1,2), (2,2),(3,3),(3,2);
+    
+     INSERT INTO course2student (student_id, course_id) VALUES (6,4);
+     
+    SELECT
+    t1.id,
+    t1.name,
+    t1.surname,
+    t3.name course_name
+    FROM stud t1
+    INNER JOIN course2student t2
+    ON t1.id = t2.student_id
+    INNER JOIN curses t3
+    ON t3.id = t2.course_id;
+    
+    CREATE TABLE contacts (
+    student_id integer unique,
+    phone long
+    );
+    
+    INSERT INTO contacts (student_id,phone) VALUES (1,1234),(2,2222),(3,3333),(4,4444);
+    INSERT INTO contacts (student_id,phone) VALUES (4,5555);
+    
+    SELECT * FROM contacts;
+    
+    SELECT 
+    t1.id,
+    t1.name,
+    t1.surname,
+    t2.phone PHONE
+    FROM stud t1
+    INNER JOIN contacts t2
+    ON t1.id = t2.student_id;
+    
+    /*
+    
+    CREATE TABLE contacts (
+    student_id integer,
+    phone varchar(128)
+    )
+    
+    INSERT INTO contacts (student_id, phone) VALUES (1,'1234'),(2,'2222'),(4,'3333'),(4,'4444');
+     SELECT 
+    t1.id,
+    t1.name,
+    t1.surname,
+    t2.phone PHONE
+    FROM stud t1
+    INNER JOIN contacts t2
+    ON t1.id = t2.student_id;
+    
+    */
+    
+    -- LEFT JOIN
+    
+      SELECT 
+    t1.id,
+    t1.name,
+    t1.surname,
+    t2.phone PHONE
+    FROM stud t1
+    LEFT JOIN contacts t2
+    ON t1.id = t2.student_id;
+    
+    -- RIGHT JOIN
+    
+	SELECT 
+    t1.id,
+    t1.name,
+    t1.surname,
+    t2.age
+    FROM stud t1
+    RIGHT JOIN ages t2
+    ON t1.id = t2.student_id;
+    
+    use hr;
+    
+    SELECT * FROM employees;
+    
+    SELECT * FROM departments;
+    
+    /*
+Task  1 . print name, surname employees and department name where he works
+*/
+    
+    SELECT
+    t1.first_name,
+    t1.last_name,
+    t2.department_name 
+    FROM employees t1
+    INNER JOIN departments t2
+    ON t1.department_id = t2.department_id;
+   
+   
+   /*
+Task  2 . print name, surname employees and department name where he works,
+but deparment must be IT, Treasury, IT Support
+*/
+    
+	SELECT
+    t1.first_name,
+    t1.last_name,
+    t2.department_name 
+    FROM employees t1
+    INNER JOIN departments t2
+    ON t1.department_id = t2.department_id
+    WHERE t2.department_name IN ('IT','Treasury','IT support');
+    
+    SELECT
+    t1.first_name,
+    t1.last_name,
+    t2.department_name 
+    FROM employees t1
+    INNER JOIN departments t2
+    ON t1.department_id = t2.department_id AND t2.department_name IN ('IT','Treasury','IT support');
+    
+    
+/*
+Task  3 . print name, surname employees and name and surname his manager
+*/
+
+-- self JOIN
+SELECT 
+  t1.first_name,
+  t1.last_name,
+  t2.first_name manager_name,
+  t2.last_name manager_last_name
+FROM employees t1
+INNER JOIN employees t2
+ON t1.manager_id = t2.employee_id;
+
+SELECT 
+  t1.first_name,
+  t1.last_name,
+  t2.first_name manager_name,
+  t2.last_name manager_last_name
+FROM employees t1
+LEFT JOIN employees t2
+ON t1.manager_id = t2.employee_id;
+
+use hr;
+
+SELECT * FROM employees;
+
+-- 1.Вывести job_id тех 
+-- сотрудников, которые зарабатывают больше своего менеджера(прием SELF join)
+
+SELECT
+t1.job_id
+FROM employees t1
+INNER JOIN employees t2
+ON t1.manager_id = t2.employee_id
+WHERE t1.salary > t2.salary;
+
+-- 2.Вывести имя, фамилию и город сотрудников, 
+-- которые работают в Seattle или Toronto (участвуют три таблицы)
+
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM countries;
+SELECT * FROM locations;
+
+SELECT
+t1.first_name,
+t1.last_name,
+t3.city
+FROM employees t1
+INNER JOIN departments t2
+ON t1.department_id = t2.department_id
+INNER JOIN locations t3
+ON t2.location_id = t3.location_id
+WHERE t3.city IN ('Seattle', 'Toronto');
+
+-- 3.Вывести имя, фамилию,
+-- название департамента и название должности сотрудника(участвуют три таблицы)
+
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM jobs;
+
+SELECT
+t1.first_name,
+t1.last_name,
+t2.department_name,
+t3.job_title
+FROM employees t1
+INNER JOIN departments t2
+ON t1.department_id = t2.department_id
+INNER JOIN jobs t3
+ON t1.job_id = t3.job_id;
+
+
+
+
+
+    
+ 
+    
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
